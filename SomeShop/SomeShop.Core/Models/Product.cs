7c536fcc4f910cb93cb5c6ef062ded8bd9a35bf7
v_n_z list.ru <v_n_z@list.ru>
2020-12-13 18:07:16 +0300

    Models and Repository in Memory

---
+++ SomeShop/SomeShop.Core/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SomeShop.Core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        [StringLength(20)]
+        [DisplayName("Product")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(0,1000)]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
