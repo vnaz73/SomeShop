106767764c0fd26a4ff7215fd2afbe77d78d8b85
v_n_z list.ru <v_n_z@list.ru>
2020-12-19 23:29:42 +0300

    Basket

---
+++ SomeShop/SomeShop.Core/Models/BasketItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SomeShop.Core.Models
+{
+    public class BasketItem : BaseEntity
+    {
+        public string BasketId { get; set; }
+        public string productId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
